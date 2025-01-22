3d2d6888e055c0bee4925bb9a708657178155fdf
Florian Cagnon <florian.cagnon@live.fr>
2025-01-22 17:28:02 +0100

    feat - verify if user was connected for access in form or error message

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,8 +1,68 @@
+import { useEffect, useState } from "react";
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import axios from "axios";
+
 export default function Register() {
+  const [pseudo, setPseudo] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [isConnected, setIsConnected] = useState(false);
+  const [error, setError] = useState('');
+
+  const checkConnection = async () => {
+    try {
+      const response = await axios.get('/api/checkIsConnected'); // Appeler l'API route
+      setIsConnected(response.data.isConnected);
+    } catch (error) {
+      console.error('Error checking connection:', error);
+      setIsConnected(false);
+    }
+  };
+
+  useEffect(() => {
+    checkConnection();
+  }, []);
 
   return (
     <div>
-      <h1>register page</h1>
+      {isConnected ? (
+        <div>
+          <p>déja connecté</p>
+        </div>
+      ) : (
+        <div>
+          <h1>register page</h1>
+          <div>
+            <form
+            >
+              <div>
+                <label htmlFor="pseudo">
+                  <p>Pseudo : </p>
+                  <input type="text" id='pseudo' value={pseudo} onChange={(e) => setPseudo(e.target.value)} />
+                </label>
+                <label htmlFor="email">
+                  <p>Adresse Email : </p>
+                  <input type="email" id='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                </label>
+                <label htmlFor="password">
+                  <p>Mot de passe : </p>
+                  <input type="password" id='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                </label>
+                <label htmlFor="confirmPassword">
+                  <p>Confirmer le mot de passe : </p>
+                  <input type="password" id='confirmPassword' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                </label>
+              </div>
+              <div>
+                <button type="submit" >Se créer un compte</button>
+              </div>
+              {/* {error && <p>{error}</p>} */}
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
