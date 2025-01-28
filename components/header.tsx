956cb59aec4f085fcde718fc2f9cb9b15854d8d6
Florian Cagnon <florian.cagnon@live.fr>
2025-01-28 16:00:56 +0100

    feat: styles and apply header in all page with layout

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,21 @@
+import styles from '../styles/header.module.css';
+import Logo from "../assets/img/logo.png";
+
 export default function Header() {
     return (
-        <div>
-            Header
+        <div className={styles.container}>
+            <div className={styles.logo}>
+                <img src={Logo.src} alt="" />
+            </div>
+            <div className={styles.buttons}>
+                <h1>L'histoire dont vous êtes le héros </h1>
+                <h1>Nouvelles</h1>
+                <h1>JDR</h1>
+                <div>
+                    <h1>co / profil</h1>
+                </div>
+                <h1>Contact</h1>
+            </div>
         </div>
     );
 }
