7d41e5ebc1e6369a6d098035277c5650f3fa621d
Florian Cagnon <florian.cagnon@live.fr>
2025-01-28 14:26:50 +0100

    feat: dev and styles

--- components/isConnectedError.tsx
+++ components/isConnectedError.tsx
@@ -1,7 +1,10 @@
 export default function IsConnectedError() {
     return (
-        <div>
-            <h1>IsConnectedError</h1>
+        <div className="formsContainer">
+            <h1>Vous ne pouvez pas accéder à cette page lorsque vous êtes connecté</h1>
+            <div className="buttonContainer">
+                <button className="button-style button-color-validate">Retourner à la page d'accueil</button>
+            </div>
         </div>
     );
 }
