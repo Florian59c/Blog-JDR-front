4ce383505755d1feaa04ea9da27aa0386d05f8c8
Florian Cagnon <florian.cagnon@live.fr>
2025-01-28 14:45:36 +0100

    feat: creation of the header and addition to all pages of the site

---
+++ components/Layout.tsx
@@ -0,0 +1,14 @@
+// components/Layout.tsx
+import React from 'react';
+import Header from './header';
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <>
+            <Header />
+            <main>{children}</main>
+        </>
+    );
+};
+
+export default Layout;
