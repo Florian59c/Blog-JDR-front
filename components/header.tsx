5fc6383bbd897487010a6a472dcabd85ac8f0823
Florian Cagnon <florian.cagnon@live.fr>
2025-01-29 14:27:24 +0100

    feat: modify header for include a responsive

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,14 @@
 import styles from '../styles/header.module.css';
 import Logo from "../assets/img/logo.png";
+import Menu from "../assets/img/burger-bar.png";
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router'; import classNames from 'classnames';
 
 export default function Header() {
     const [isConnected, setIsConnected] = useState(false);
+    const [displayHeader, setDisplayHeader] = useState(false);
     const router = useRouter();
 
     const checkConnection = async () => {
@@ -36,35 +38,68 @@ export default function Header() {
     }, [router]); // exécution lors d'un changement de route
 
     return (
-        <div className={styles.container}>
-            <div className={styles.logo}>
-                <Link href="/">
-                    <img src={Logo.src} alt="le logo du site" />
-                </Link>
-            </div>
-            <div className={styles.buttons}>
-                <Link href="/yourHeroStory">
-                    <h1>L'histoire dont vous êtes le héros </h1>
-                </Link>
-                <Link href="/news">
-                    <h1>Nouvelles</h1>
-                </Link>
-                <Link href="/jdr">
-                    <h1>JDR</h1>
-                </Link>
-                {isConnected ? (
-                    <Link href="/profile">
-                        <h1>Profil</h1>
+        <div className={styles.header}>
+            <div className={styles.container}>
+                <div className={styles.logoContainer}>
+                    <Link href="/">
+                        <img src={Logo.src} alt="le logo du site" className={styles.logo} />
                     </Link>
-                ) : (
-                    <Link href="/login">
-                        <h1>Se connecter</h1>
+                </div>
+                <img
+                    src={Menu.src}
+                    alt="Menu"
+                    className={classNames(styles.displayMobile, styles.menu)}
+                    onClick={() => { setDisplayHeader(!displayHeader) }}
+                />
+                <div className={classNames(styles.buttons, styles.displayPC)}>
+                    <Link href="/yourHeroStory">
+                        <h1>L'histoire dont vous êtes le héros </h1>
                     </Link>
-                )}
-                <Link href="/contact">
-                    <h1>Contact</h1>
-                </Link>
-            </div>
+                    <Link href="/news">
+                        <h1>Nouvelles</h1>
+                    </Link>
+                    <Link href="/jdr">
+                        <h1>JDR</h1>
+                    </Link>
+                    {isConnected ? (
+                        <Link href="/profile">
+                            <h1>Profil</h1>
+                        </Link>
+                    ) : (
+                        <Link href="/login">
+                            <h1>Se connecter</h1>
+                        </Link>
+                    )}
+                    <Link href="/contact">
+                        <h1>Contact</h1>
+                    </Link>
+                </div>
+            </div >
+            {displayHeader && (
+                <div className={classNames(styles.buttons, styles.displayMobile)}>
+                    <Link href="/yourHeroStory">
+                        <h1>L'histoire dont vous êtes le héros </h1>
+                    </Link>
+                    <Link href="/news">
+                        <h1>Nouvelles</h1>
+                    </Link>
+                    <Link href="/jdr">
+                        <h1>JDR</h1>
+                    </Link>
+                    {isConnected ? (
+                        <Link href="/profile">
+                            <h1>Profil</h1>
+                        </Link>
+                    ) : (
+                        <Link href="/login">
+                            <h1>Se connecter</h1>
+                        </Link>
+                    )}
+                    <Link href="/contact">
+                        <h1>Contact</h1>
+                    </Link>
+                </div>
+            )}
         </div>
     );
 }
