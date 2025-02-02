7688b1d07fb298a6d9b1350a44c36a26b2ceea33
Florian Cagnon <florian.cagnon@live.fr>
2025-02-02 17:35:10 +0100

    feat: changes names

---
+++ pages/yourHeroStories.tsx
@@ -0,0 +1,7 @@
+export default function yourHeroStories() {
+    return (
+        <div>
+            <h1>Histoires dont vous êtes le héros</h1>
+        </div>
+    );
+}
--- pages/yourHeroStory.tsx
+++
@@ -1,7 +0,0 @@
-export default function YourHeroStory() {
-    return (
-        <div>
-            <h1>YourHeroStory</h1>
-        </div>
-    );
-}
