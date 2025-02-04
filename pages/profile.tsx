c528f6aba3271c5b6899a7c22355ee2ab07c2d3e
Florian Cagnon <florian.cagnon@live.fr>
2025-02-04 14:58:23 +0100

    feat: check if the user is logged in when accessing the profile page and redirect to login otherwise

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -13,6 +13,16 @@ export default function Profile() {
     const [confirmMessage, setconfirmMessage] = useState('');
     const router = useRouter();
 
+    const checkConnection = async () => {
+        try {
+            const response = await axios.get('/api/checkIsConnected'); // Appeler l'API route
+            setIsConnected(response.data.isConnected);
+        } catch (error) {
+            console.error('Error checking connection:', error);
+            setIsConnected(false);
+        }
+    };
+
     async function getCurrentUser() {
         try {
             const response = await axios.post(
@@ -31,79 +41,84 @@ export default function Profile() {
     }
 
     useEffect(() => {
+        checkConnection();
         getCurrentUser();
     }, []);
 
     return (
         <div>
-            <div className="blockContainer">
-                <h1>Page de profil</h1>
-                <div>
-                    <h1>logout</h1>
-                    <button onClick={async () => {
-                        await axios.post(
-                            `${process.env.NEXT_PUBLIC_SERVER_URL}auth/logout`, {}, { withCredentials: true });
-                        router.push('/login');
-                    }}>
-                        Se déconnecter
-                    </button>
-                </div>
-
-
-
-                <div>
-                    <form
-                        onSubmit={async (e) => {
-                            e.preventDefault(); // Empêche le rechargement de la page
-                            setconfirmMessage('');
-                            setError('');
-                            try {
-                                const response = await axios.post(
-                                    `${process.env.NEXT_PUBLIC_SERVER_URL}user/updateUser`,
-                                    { pseudo, email },
-                                    {
-                                        withCredentials: true,
+            {isConnected ? (
+                <div className="blockContainer">
+                    <h1>Page de profil</h1>
+                    <div>
+                        <h1>logout</h1>
+                        <button onClick={async () => {
+                            await axios.post(
+                                `${process.env.NEXT_PUBLIC_SERVER_URL}auth/logout`, {}, { withCredentials: true });
+                            router.push('/login');
+                        }}>
+                            Se déconnecter
+                        </button>
+                    </div>
+                    <div>
+                        <form
+                            onSubmit={async (e) => {
+                                e.preventDefault(); // Empêche le rechargement de la page
+                                setconfirmMessage('');
+                                setError('');
+                                try {
+                                    const response = await axios.post(
+                                        `${process.env.NEXT_PUBLIC_SERVER_URL}user/updateUser`,
+                                        { pseudo, email },
+                                        {
+                                            withCredentials: true,
+                                        }
+                                    )
+                                    if (response.data === "ok") {
+                                        setconfirmMessage("Votre profil a été modifié");
+                                    } else {
+                                        setError(response.data);
                                     }
-                                )
-                                if (response.data === "ok") {
-                                    setconfirmMessage("Votre profil a été modifié");
-                                } else {
-                                    setError(response.data);
+                                } catch (error) {
+                                    console.error(error);
+                                    setError("Une erreur inconnue s'est produite");
                                 }
-                            } catch (error) {
-                                console.error(error);
-                                setError("Une erreur inconnue s'est produite");
-                            }
-                        }}
-                    >
-                        <div className="inputs">
-                            <TextField
-                                label="Pseudo"
-                                type="text"
-                                autoComplete="text"
-                                value={pseudo}
-                                onChange={(e) => setPseudo(e.target.value)}
-                            />
-                            <TextField
-                                label="Adresse Email"
-                                type="email"
-                                autoComplete="email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </div>
-                        {error && <p className="error-message">{error}</p>}
-                        <div className="buttonContainer">
-                            <button type="submit" className="button-style button-color-validate">Modifier mon profil</button>
-                        </div>
-                    </form>
-                    <p className="confirmMessage">{confirmMessage}</p>
+                            }}
+                        >
+                            <div className="inputs">
+                                <TextField
+                                    label="Pseudo"
+                                    type="text"
+                                    autoComplete="text"
+                                    value={pseudo}
+                                    onChange={(e) => setPseudo(e.target.value)}
+                                />
+                                <TextField
+                                    label="Adresse Email"
+                                    type="email"
+                                    autoComplete="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </div>
+                            {error && <p className="error-message">{error}</p>}
+                            <div className="buttonContainer">
+                                <button type="submit" className="button-style button-color-validate">Modifier mon profil</button>
+                            </div>
+                        </form>
+                        <p className="confirmMessage">{confirmMessage}</p>
+                    </div>
                 </div>
-            </div>
-
-
-
-
+            ) : (
+                <div className="blockContainer">
+                    <h1>Vous ne pouvez pas accéder à cette page si vous n'êtes pas connecté</h1>
+                    <div className="buttonContainer">
+                        <Link href="/login">
+                            <button className="button-style button-color-validate">Se connecter</button>
+                        </Link>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
