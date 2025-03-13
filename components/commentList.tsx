2f44ce03893873a7c45f89e5e510b8f0fa1449b6
Florian Cagnon <florian.cagnon@live.fr>
2025-03-13 15:45:51 +0100

    feat: styles for the component

--- components/commentList.tsx
+++ components/commentList.tsx
@@ -1,7 +1,10 @@
+import styles from '../styles/commentList.module.css';
 import { useEffect, useState } from "react";
 import { CommentPropsInterface } from "../interfaces/CommentPropsInterface";
 import { CommentsInterface } from "../interfaces/CommentsInterface";
 import axios from "axios";
+import Exclamation from "../assets/img/exclamation.png";
+import { Tooltip } from '@mui/material';
 
 export default function CommentList({ id, pageType, refreshComments }: CommentPropsInterface & { refreshComments: number }) {
     const [comments, setComments] = useState<CommentsInterface[]>([]);
@@ -29,27 +32,31 @@ export default function CommentList({ id, pageType, refreshComments }: CommentPr
     return (
         <div>
             {comments.length === 0 ? (
-                <p>Ce post n'a pas de commentaire</p>
+                <p className={styles.errorMessage}>Ce post n'a pas de commentaire</p>
             ) : (
-                <div>
+                <div className={styles.container}>
                     {comments.map((comment) => {
                         return (
-                            <div key={comment.id}>
-                                <p>
-                                    Créé le {new Date(comment.creation_date).toLocaleDateString()} à {new Date(comment.creation_date).
-                                        toLocaleTimeString("fr-FR", {
-                                            hour: "2-digit",
-                                            minute: "2-digit",
-                                        })}
-                                </p>
-                                <p>{comment.user.pseudo}</p>
-                                <p>{comment.content}</p>
+                            <div key={comment.id} className={styles.commentContainer}>
+                                <div className={styles.commentHeader}>
+                                    <p>
+                                        Créé par <span className={styles.boldText}>{comment.user.pseudo}</span>, le {new Date(comment.creation_date)
+                                            .toLocaleDateString()} à {new Date(comment.creation_date)
+                                                .toLocaleTimeString("fr-FR", {
+                                                    hour: "2-digit",
+                                                    minute: "2-digit",
+                                                })}
+                                    </p>
+                                    <Tooltip title="Signaler le commentaire" placement="top" arrow>
+                                        <img src={Exclamation.src} alt="Signaler le commentaire" />
+                                    </Tooltip>
+                                </div>
+                                <p className={styles.content}>{comment.content}</p>
                             </div>
                         )
                     })}
                 </div>
-            )
-            }
+            )}
         </div >
     );
 }
