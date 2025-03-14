6eac1acb7ac5957f8bc185f99f043cae78dd1d57
Florian Cagnon <florian.cagnon@live.fr>
2025-03-14 17:49:47 +0100

    feat: reload component after delete a comment

--- pages/userComments.tsx
+++ pages/userComments.tsx
@@ -39,7 +39,7 @@ export default function UserComments() {
     useEffect(() => {
         checkConnection();
         getCurrentUserComments();
-    }, []);
+    }, [isOpen]);
 
     return (
         <div>
