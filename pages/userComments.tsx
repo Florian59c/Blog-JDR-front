2f72dab844676ba41b39a52089a351e35c5a768d
Florian Cagnon <florian.cagnon@live.fr>
2025-03-18 15:19:27 +0100

    feat: modify comment form and button styles

--- pages/userComments.tsx
+++ pages/userComments.tsx
@@ -5,12 +5,15 @@ import { useEffect, useState } from "react";
 import Arrow from '../assets/img/return-arrow.png';
 import { CommentsInterface } from '../interfaces/CommentsInterface';
 import DeleteModale from "../modals/deleteModale";
+import { TextField } from '@mui/material';
 
 export default function UserComments() {
     const [isConnected, setIsConnected] = useState(false);
     const [comments, setComments] = useState<CommentsInterface[]>([]);
     const [isOpen, setIsOpen] = useState(false);
     const [deleteId, setDeleteId] = useState<number>(0);
+    const [content, setContent] = useState<{ [key: number]: string }>({});
+    const [canModify, setCanModify] = useState<{ [key: number]: boolean }>({});
 
     async function checkConnection() {
         try {
@@ -49,13 +52,14 @@ export default function UserComments() {
                         <img src={Arrow.src} alt="flèche" />
                         <p>retourner sur la page de profil</p>
                     </Link>
-                    <div className='comments-block'>
+                    <div className="comments-block">
                         <h1>Mes commentaires</h1>
                         {comments.length === 0 ? (
                             <p className="comment-error-message">Vous n'avez pas écrit de commentaire</p>
                         ) : (
                             <div>
                                 {comments.map((comment) => {
+                                    const isEditing = canModify[comment.id] || false;
                                     return (
                                         <div key={comment.id} className={`commentContainer ${styles.displayContent}`}>
                                             <div className={styles.content}>
@@ -66,10 +70,67 @@ export default function UserComments() {
                                                             minute: "2-digit",
                                                         })} :
                                                 </p>
-                                                <p>{comment.content}</p>
+                                                {isEditing ? (
+                                                    <form
+                                                    // onSubmit={async (e) => {
+                                                    //     e.preventDefault(); // Empêche le rechargement de la page
+                                                    //     setError('');
+                                                    //     setconfirmMessage('');
+                                                    //     try {
+                                                    //         const response = await axios.post(
+                                                    //             `${process.env.NEXT_PUBLIC_SERVER_URL}comment/createComment`,
+                                                    //             { content, postType: pageType, postId: id },
+                                                    //             { withCredentials: true } // Nécessaire pour inclure les cookies
+                                                    //         );
+                                                    //         if (response.data === "ok") {
+                                                    //             onCommentAdded(); // Déclenche la mise à jour de CommentList
+                                                    //             setconfirmMessage('Votre commentaire a bien été créé');
+                                                    //         } else {
+                                                    //             setError(response.data);
+                                                    //         }
+                                                    //     } catch (error) {
+                                                    //         if (axios.isAxiosError(error)) {
+                                                    //             // Si l'erreur provient d'Axios ou des dto
+                                                    //             setError(error.response?.data?.message || 'Une erreur est survenue lors de la création du commentaire');
+                                                    //         } else {
+                                                    //             setError('Une erreur inconnue s\'est produite');
+                                                    //         }
+                                                    //     }
+                                                    // }}
+                                                    >
+                                                        <TextField
+                                                            id="outlined-multiline-static"
+                                                            multiline
+                                                            rows={3}
+                                                            type="text"
+                                                            variant="outlined"
+                                                            value={content[comment.id] || comment.content}
+                                                            onChange={(e) =>
+                                                                setContent({ ...content, [comment.id]: e.target.value })
+                                                            }
+                                                        />
+                                                        <div className={styles.modifyButton}>
+                                                            <button type="submit" className="button-style button-color-validate">
+                                                                Modifier le commentaire
+                                                            </button>
+                                                        </div>
+                                                    </form>
+                                                ) : (
+                                                    <p>{comment.content}</p>
+                                                )}
                                             </div>
                                             <div className={styles.buttons}>
-                                                <button className="button-style button-color-validate">Modifier</button>
+                                                <button
+                                                    className="button-style button-color-validate"
+                                                    onClick={() =>
+                                                        setCanModify((prev) => ({
+                                                            ...prev,
+                                                            [comment.id]: !isEditing
+                                                        }))
+                                                    }
+                                                >
+                                                    {isEditing ? "Ne plus modifier" : "Modifier"}
+                                                </button>
                                                 <button
                                                     className="button-style button-color-error"
                                                     onClick={() => { setDeleteId(comment.id); setIsOpen(true) }}
@@ -78,7 +139,7 @@ export default function UserComments() {
                                                 </button>
                                             </div>
                                         </div>
-                                    )
+                                    );
                                 })}
                             </div>
                         )}
