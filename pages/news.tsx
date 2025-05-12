a077b2df1425abc4bfb3a7d5d14b8011d8d8d852
Florian Cagnon <florian.cagnon@live.fr>
2025-05-12 16:57:26 +0200

    feat: news page vue for mobile and tablet

--- pages/news.tsx
+++ pages/news.tsx
@@ -2,9 +2,26 @@ import styles from '../styles/news.module.css';
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { NewsInterface } from "../interfaces/NewsInterface";
+import RightArrow from "../assets/img/right-arrow.png";
+import classNames from 'classnames';
+import CommentForm from '../components/commentForm';
+import CommentList from '../components/commentList';
 
 export default function News() {
+    const pageType = "news";
     const [news, setNews] = useState<NewsInterface[]>([]);
+    const [activeIds, setActiveIds] = useState<number[]>([]);
+    const [refreshComments, setRefreshComments] = useState(0);
+
+    function handleCommentAdded() {
+        setRefreshComments((prev) => prev + 1); // Incrémente l'état pour forcer le rafraîchissement
+    }
+
+    function toggleActive(id: number) {
+        setActiveIds((prev) =>
+            prev.includes(id) ? prev.filter((activeId) => activeId !== id) : [...prev, id]
+        );
+    }
 
     async function getHeroStories() {
         try {
@@ -19,20 +36,58 @@ export default function News() {
         getHeroStories();
     }, []);
 
+    const formatDriveLink = (link: string) => {
+        return link.replace(/\/view\?.*$/, "/preview");
+    };
+
     return (
         <div>
             {news.length === 0 ? (
                 <p className={styles.errorMessage}>Nous n'avons trouvé aucune nouvelle</p>
             ) : (
                 <div>
-                    <div className={styles.displayMobile}>
-                        <p>vue mobile</p>
+                    <div className={classNames(styles.displayMobile, 'blockContainer')}>
+                        {news.map((newsItem) => {
+                            const isActive = activeIds.includes(newsItem.id);
+                            return (
+                                <div key={newsItem.id}>
+                                    <div className={styles.newsHeader} onClick={() => toggleActive(newsItem.id)}>
+                                        <h1 className={isActive ? styles.active : ''}>{newsItem.title}</h1>
+                                        <img src={RightArrow.src} alt="flèche" className={classNames(styles.arrow, { [styles.arrowActive]: isActive })} />
+                                    </div>
+                                    {isActive && (
+                                        <div className={styles.newsContent}>
+                                            <p className={styles.date}>
+                                                Ajouté le {new Date(newsItem.date)
+                                                    .toLocaleDateString()} à {new Date(newsItem.date)
+                                                        .toLocaleTimeString("fr-FR", {
+                                                            hour: "2-digit",
+                                                            minute: "2-digit",
+                                                        })}
+                                            </p>
+                                            <div className={styles.pdf}>
+                                                <iframe
+                                                    src={formatDriveLink(newsItem.link)}
+                                                />
+                                            </div>
+                                            <div>
+                                                <CommentForm id={newsItem.id} pageType={pageType} onCommentAdded={handleCommentAdded} />
+                                                <div className={styles.comments}>
+                                                    <CommentList id={newsItem.id} pageType={pageType} refreshComments={refreshComments} />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
+                            );
+                        })}
                     </div>
                     <div className={styles.displayPC}>
                         <p>vue pc</p>
                     </div>
                 </div>
-            )}
-        </div>
+            )
+            }
+        </div >
     );
 }
