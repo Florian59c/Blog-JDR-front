405f545d00c37b8036b619d2c3638d0feabd001e
Florian Cagnon <florian.cagnon@live.fr>
2025-05-16 15:26:01 +0200

    feat: start for adminpage vue

--- pages/MJ/adminPage.tsx
+++ pages/MJ/adminPage.tsx
@@ -1,8 +1,39 @@
+import Link from "next/link";
+
 export default function AdminPage() {
+  const pages = [
+    {
+      title: 'Commentaires signalés',
+      link: 'reportedComments'
+    },
+    {
+      title: 'Histoires dont vous êtes le héros',
+      link: 'yourHeroStoriesAdmin'
+    },
+    {
+      title: 'Nouvelles',
+      link: 'newsAdmin'
+    },
+    {
+      title: 'JDR',
+      link: 'jdrAdmin'
+    }
+  ];
+
   return (
     <div>
-      <h1>Page d'administration</h1>
-      <p>Bienvenue, administrateur.</p>
+      <h1>Page d'accueil administrateur</h1>
+      <div>
+        {pages.map((page, index) => {
+          return (
+            <Link href={`/MJ/${page.link}`} key={index}>
+              <button>
+                {page.title}
+              </button>
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 }
