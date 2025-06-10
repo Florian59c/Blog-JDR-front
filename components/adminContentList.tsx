c94d1a5252e284b5e8d5b22f7eb921f176af86f2
Florian Cagnon <florian.cagnon@live.fr>
2025-06-10 16:33:45 +0200

    feat: component for diplay list with modify and delete buttons

--- components/adminContentList.tsx
+++ components/adminContentList.tsx
@@ -1,7 +1,62 @@
+import styles from '../styles/adminContentList.module.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { HeroStoryInterface } from "../interfaces/HeroStoryInterface";
+import { Button } from "@mui/material";
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import DeleteIcon from "@mui/icons-material/Delete";
+
 export default function AdminContentList() {
+    const [contents, setContents] = useState<HeroStoryInterface[]>([]);;
+
+    async function getHeroStories() {
+        try {
+            const response = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}hero/getAllHeroWithNewDate`,);
+            setContents(response.data);
+        } catch (error) {
+            console.error('Error checking connection:', error);
+        }
+    }
+
+    useEffect(() => {
+        getHeroStories();
+    }, []);
+
     return (
         <div>
-            <p>Ici s'affiche la liste des histoires dont vous êtes le héros.</p>
+            {contents.map((content) => {
+                return (
+                    <div className={`commentContainer ${styles.container}`}>
+                        <p className={styles.date}>
+                            Ajouté le {new Date(content.date)
+                                .toLocaleDateString()} à {new Date(content.date)
+                                    .toLocaleTimeString("fr-FR", {
+                                        hour: "2-digit",
+                                        minute: "2-digit",
+                                    })}
+                        </p>
+                        <p className={styles.title}>{content.title}</p>
+                        <div className={styles.buttons}>
+                            <Button
+                                variant="outlined"
+                                color="success"
+                                sx={{ width: '100%' }}
+                                startIcon={<EditOutlinedIcon />}
+                            >
+                                Modifier
+                            </Button>
+                            <Button
+                                variant="outlined"
+                                color="error"
+                                sx={{ width: '100%' }}
+                                startIcon={<DeleteIcon />}
+                            >
+                                Supprimer
+                            </Button>
+                        </div>
+                    </div>
+                );
+            })}
         </div>
     );
 }
