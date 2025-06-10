15f2a750745c221511e800e8a0797012a2990d49
Florian Cagnon <florian.cagnon@live.fr>
2025-06-10 15:54:52 +0200

    feat: start of hero page whith components

---
+++ components/adminContentList.tsx
@@ -0,0 +1,7 @@
+export default function AdminContentList() {
+    return (
+        <div>
+            <p>Ici s'affiche la liste des histoires dont vous êtes le héros.</p>
+        </div>
+    );
+}
